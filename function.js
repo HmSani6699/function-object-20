9e99cae4c0ad993da12c7b3739856f8d8bb63fb1
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-30 13:33:42 +0600

    ad to my name

--- function.js
+++ function.js
@@ -2,4 +2,16 @@ function sum(num1, num2) {
     var total = num1 + num2;
     console.log(total)
 }
-sum(10, 30);
+// sum(10, 30);
+
+
+function fullName(first, last) {
+    var firstName = first;
+    var lastName = last;
+    const fullName = firstName + ' ' + lastName;
+    return fullName
+}
+
+
+var myName = fullName('Md', 'Sadiq');
+console.log(myName)
