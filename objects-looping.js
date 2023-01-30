5fe7393c36da0130f5bf9f1252600ae147eb483b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-30 22:21:50 +0600

    ad to objects in for loop

---
+++ objects-looping.js
@@ -0,0 +1,30 @@
+var man = {
+    name: 'manos',
+    hat: 2,
+    matha: 1,
+    kan: 2,
+    nak: 1
+}
+
+//objects property name
+var propertyName = Object.keys(man);
+// console.log(propertyName);
+
+//objects property value
+var valueName = Object.values(man);
+// console.log(valueName);
+
+// objects property =[ 'name', 'hat', 'matha', 'kan', 'nak' ]
+
+for (var i = 0; i < propertyName.length; i++) {
+    var pName = propertyName[i];
+    var vName = man[pName]
+    // console.log(pName, vName)
+}
+
+//for in loop 
+
+for (var propertyName in man) {
+    var value = man[propertyName];
+    // console.log(propertyName, value)
+}
